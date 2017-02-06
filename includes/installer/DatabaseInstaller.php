ae131cca10652b581505bdd6259a760b3801eb12
dkboss <deepakvermaasb@gmail.com>
2017-02-06 18:21:01 +0530

    some correction

--- includes/installer/DatabaseInstaller.php
+++ includes/installer/DatabaseInstaller.php
@@ -13,29 +13,30 @@ function form() {
 <head>
 <meta charset="UTF-8"/>
 <title>MySql Database settings</title>
-style 
-table{
-
-
-
-}
 
 </head>
 
 <body >
+
 <h2>This Software require MySql access to create and maintain databases.<br/>If you have not installed it, Please do it now.</h2>
+
 <h3>Please Enter MySql Database information.</h3>
 
 <form action="includes/installer/MySqlInstaller.php" method="post">
+
+
 <table border = "15">
+
 <caption style ="color:blue;text-align:center"><h1><b>WELCOME</b></h1></CAPTION>
 <tr><th>Database Client :</th> <td><input type="text" name="dbclient" value="mysql"/></td></tr> 
 <tr><th>Database Server :</th><td> <input type="text" name="dbserver" value="localhost:3306" /> </td></tr>
 <tr><th>Database Name   : </th><td><input type="text" name="dbname" value="eorgDB" /></td> </tr>
 <tr><th>MySql Username  :</th><td> <input type="text" name="mysqlUser" value="root" /> </td></tr>
-<tr><th>MySql Password  : </th><td><input type="password" name="mysqlPasswd" /> </td></tr>
+<tr><th>MySql Password  : </th><td><input type="password" name="mysqlPasswd"/> </td></tr>
+
 </table>
-<input style="text-aling :center" type="submit" name="submit" value="Submit!" />
+
+<input  type="submit" name="submit" value="Submit!" />
 </form>
  
 </body>
