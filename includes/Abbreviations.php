071c2a4aabecefe49b26c366f24ea23be0b32a9d
DKBOSS <deepakvermaasb@gmail.com>
2017-03-01 13:19:17 +0530

    modification in adduser

--- includes/Abbreviations.php
+++ includes/Abbreviations.php
@@ -11,7 +11,15 @@
 $Abbr['Faculty'] = 'F' ; $DAbbr['F'] = 'Faculty' ;
 $Abbr['Student'] = 'S' ; $DAbbr['S'] = 'Student' ;
 
-$Abbr['Faculty Adviser'] = 'FA' ; $DAbbr['FA'] = 'Faculty Adviser' ;
+//other Acedmic Rank
+
+$AbbrOArnk['Faculty Adviser'] = 'FA' ; $DAbbrOArnk['FA'] = 'Faculty Adviser' ;
+$AbbrOArnk['Head Of Department'] = 'HOD'; $DAbbrOArnk['HOD'] = 'Head Of Department' ;
+
+
+
+
+
 
 $Abbr['Academic'] = 'AC' ; $DAbbr['AC'] = 'Academic' ;
 
@@ -29,9 +37,18 @@ $AppStat['A'] = 'Approved';
 $AppStat['P'] = 'Pending';
 $AppStat['Y'] = 'Accepted';
 
+
 //abbreviations degree
 $AbbrDeg['BTech'] = 'B';  $DAbbrDeg['B'] = 'BTech';
 $AbbrDeg['MTech'] = 'M';  $DAbbrDeg['M'] = 'MTech';
 $AbbrDeg['PhD']   = 'P';  $DAbbrDeg['P'] = 'PhD';
+
+
+//Academic ranks
+$AbbrArnk['professor'] = 'prof'; $DAbbrArnk['prof'] = 'professor';
+$AbbrArnk['Associate Professor'] = 'AsProf'; $DAbbrArnk['AsProf'] = 'Associate Professor';
+$AbbrArnk['Assistant Professor'] = 'AProf' ; $DAbbrArnk['AProf'] = 'Assistant Professor';
+
+
 
 ?>
--- includes/option.php
+++ includes/option.php
@@ -24,34 +24,37 @@ $html = <<<HTML
 <tr><th>Password : </th><td><input type="text" name="passwd" required/> </td></tr>
 <tr><th>Full Name : </th><td><input type="text" name="fname" required/> </td></tr>
 </table>
-Designations : Student 
-<br>
+<br><input type ="radio" name = "design" value = "student">Student ::
+Degree:
 <select name ="std">
+<option value = > </option>
 HTML;
 echo"$html";
 
-foreach ($AbbrDeg as $a )
+foreach($AbbrDeg as $a )
 {
 echo"<option value =".$a.">".$DAbbrDeg[$a]."</option>";
 }
 
-
-$html1 = <<<HTML1
+$html = <<<HTML
 </select>
+department:
 <select name = "dcpln">
-HTML1;
+<option value = > </option>
+HTML;
 
 
-echo "$html1";
+echo "$html";
 
-foreach ($AbbrDept as $a ){
+foreach($AbbrDept as $a ){
 echo"<option value =".$a.">".$DAbbrDept[$a]."</option>";
 }
 
-$html2 = <<<HTML2
+$html = <<<HTML
 </select>
-<br>
+Batch:
 <select name = "batch">
+<option value = > </option>
 <option value ="05">2005</option>
 <option value ="06">2006</option>
 <option value ="07">2007</option>
@@ -72,12 +75,64 @@ $html2 = <<<HTML2
 <option value ="22">2022</option>
 <option value ="23">2023</option>
 </select>
-<br><input  type="submit" name="submit" value="Create User !" />
+<br><hr>
+<Br>
+<input type ="radio" name = "design" value = "faculty">Faculty ::
+Department:
+<select name = "dcpln">
+<option value = > </option>
+HTML;
+
+
+echo "$html";
+
+foreach($AbbrDept as $a ){
+echo"<option value =".$a.">".$DAbbrDept[$a]."</option>";
+}
+
+$html = <<<HTML
+</select>
+Acedmic Rank:
+<select name = "Arnk" >
+<option value = > </option>
+HTML;
+
+echo "$html";
+
+foreach($AbbrArnk as $a)
+{
+echo"<option value =".$a.">".$DAbbrArnk[$a]."</option>";
+}
+
+$html = <<<HTML
+</select>
+<br><br>
+Other Rank::
+<input type = "radio" name = "OArnk" value = "yes" >Yes
+<input type = "radio" name = "OArnk" value = "No" >No <br>
+if yes then select:
+<select name = "OArank" >
+<option value = > </option>
+HTML;
+
+echo "$html";
+
+
+
+foreach($AbbrOArnk as $a )
+{
+echo"<option value =".$a.">".$DAbbrOArnk[$a]."</option>";
+}
+
+$html = <<<HTML
+</select>
+<br><br><br>
+<input  type="submit" name="submit" value="Create User !" />
 </form>
  
 </body>
 </html>
-HTML2;
+HTML;
 
-echo "$html2";
+echo "$html";
 ?>
