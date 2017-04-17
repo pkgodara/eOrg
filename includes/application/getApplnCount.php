a5c307a5bf8a042db01632eaf6909130fad84d77
DKBOSS <deepakvermaasb@gmail.com>
2017-04-18 04:54:02 +0530

    minor bug fix

--- includes/application/getApplnCount.php
+++ includes/application/getApplnCount.php
@@ -33,22 +33,71 @@ echo <<<HTML
 <html lang="en">
 <head>
 <title>Application Count</title>
+<link rel="shortcut icon" href="/favicon.png" type="image/png">
+<link rel="shortcut icon" type="image/png" href="../../image/gogreen.jpg" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+
+
+body {
+    color:white;
+    background-image: url("../../image/image4.jpg");
+     min-height: 500px;
+    background-attachment: fixed;
+    background-position: center;
+   
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+table, td, th {
+    text-align:center;
+    border: 1px solid black;
+}
+
+table {
+    width: 100%;
+    border-color: white;
+}
+
+th {
+    height: 50px;
+}
+
+
+input[type=button],select
+{
+	cursor: pointer; font-size : 25px; height:auto; width:auto ;background-color:black;color:white ;
+	border: 0.25px solid white;
+}
+button{
+	cursor: pointer; font-size : 25px; height:auto; width:auto ;background-color:transparent;color:white ;
+	border: 0.25px solid white;
+}
+
+
+
+
+</style>
+
+
+
 
 </head>
-
-<body >
+<body ><br><br>
+     <button onclick="document.location.href='handleAppl.php'" > BACK </button>
+<center><br><br><br><br><br><br>
 
 <h2>Please Select Date or Month or Year to get count</h2>
-
-Select Date : <input type="date" name="date" id="date" placeholder="yyyy-mm-dd" required/>
 <br>
+Select Date : <input type="date" name="date" id="date" placeholder="yyyy-mm-dd" required/>
+<br><br>
 Applications on : 
 <select id="choice">
 <option value="year" >Year</option>
 <option value="month" >Month</option>
 <option value="day" selected>Full Date</option>
 </select>
-<br>
+<br><br><br><br><br><br><br>
 <button name="submit" id="submit" >Submit Application !</button>
 
 HTML;
@@ -60,9 +109,11 @@ echo "<input type=\"text\" name=\"usr\" id=\"usr\" value=$user style=\"visibilit
 echo <<<HTML
 
 <div id="table">
-<table>
-<caption style ="color:blue;text-align:center">Your Total Applications Till Now</caption>
 
+<h2 style ="color:white;text-align:center">Your Total Applications Till Now</h2>
+<table><br>
+
+<br>
 <tr>
 <td>Application type</td>
 <td>Total No of App.</td>
